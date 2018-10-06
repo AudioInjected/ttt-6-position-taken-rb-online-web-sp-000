7b5fb6f33ba7bf446159da4d77c2734f2b200467
Rafael Batista <rafaelb24@yahoo.com>
2018-10-06 01:55:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
-ear# code your #position_taken? method here!
+# code your #position_taken? method here!
 def position_taken?(board, index)
  if board[index] == " " || board[index] == "" || board[index] == nil
     false
