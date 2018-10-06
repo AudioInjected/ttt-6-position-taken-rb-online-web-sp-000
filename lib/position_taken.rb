901465247767f70fe84cf765f2b63775c1004992
Rafael Batista <rafaelb24@yahoo.com>
2018-10-06 01:53:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,10 @@
-# code your #position_taken? method here!
+ear# code your #position_taken? method here!
 def position_taken?(board, index)
-<<<<<<< HEAD
  if board[index] == " " || board[index] == "" || board[index] == nil
     false
  elsif board[index] == "X" || board[index] == "O"
     true
  end
-=======
   if board[index] == " " || ""
     false
 #  elsif board[index] == ""
@@ -16,5 +14,4 @@ def position_taken?(board, index)
   elsif board[index] == "X" || "O"
     true
   end
->>>>>>> 69b5e5e6cc5342ea17b277b58f6463920cfd38a2
 end
